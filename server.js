95b99c1cc56b207fefb998e7cf9409043947b5c4
LongBeachHXC <fweinberg@live.com>
2018-02-27 20:33:22 -0800

    Server building

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
