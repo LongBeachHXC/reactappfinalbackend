c96bf2854ee58f73bbfcf231e3a25b8799705fff
LongBeachHXC <fweinberg@live.com>
2018-03-01 07:50:00 -0800

    Integrating Database

--- server.js
+++ server.js
@@ -100,13 +100,12 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        console.log(entries);
+    db('').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
+        res.json(entries[0]);
     })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
+
 app.listen(3001, ()=> {
     console.log('app is running on port 3001');
 })
