0e69a7d7964d11e0e3decedf59e67dc4f2b8626c
LongBeachHXC <fweinberg@live.com>
2018-03-01 08:49:12 -0800

    Integrating Database

--- server.js
+++ server.js
@@ -66,8 +66,18 @@ app.post('/signin', (req, res) => {
     db.select('email', 'hash').from('login')
         .where('email', '=', req.body.email)
         .then(data => {
-            console.log(data);
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    console.log(user);
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('unable to get user'))
+            }
         })
+        .catch(err => res.status(400).json('Wrong Credentials'))
 })
 
 app.post('/register', (req, res) => {
