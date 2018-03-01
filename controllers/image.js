f537534d338d599c97eab1c72333fa7599a2a2e6
LongBeachHXC <fweinberg@live.com>
2018-03-01 10:06:47 -0800

    Integrating Database

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,4 @@
-const handleImage(req, res, db) => {
+const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
         res.json(entries[0]);
