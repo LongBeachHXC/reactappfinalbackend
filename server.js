11bd6b5a29cd3239e168d3899e78c355781858be
LongBeachHXC <fweinberg@live.com>
2018-03-01 14:36:17 -0800

    Added proccess.env.PORT

--- server.js
+++ server.js
@@ -24,11 +24,12 @@ const db = knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    db.select('*').from('users').then(tableUsers => {
-        res.json(tableUsers)
-    })
-})
+app.get('/', (req, res) => { res.send('I AM ALIVE!') })
+
+    // db.select('*').from('users').then(tableUsers => {
+    //     res.json(tableUsers)
+    // })
+
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
