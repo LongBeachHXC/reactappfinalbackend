0943d50268961803f3ad3420dcddb523e357f4f4
LongBeachHXC <fweinberg@live.com>
2018-03-01 11:46:56 -0800

    Integrating Database

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd78f3a9a7ea0403db43d98d2a70bfe7e'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(
+            Clarifai.FACE_DETECT_MODEL,
+            req.body.input)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
