49f9804ced9208902844a89ec69cd50b3c8712cd
LongBeachHXC <fweinberg@live.com>
2018-03-01 00:10:09 -0800

    Integrating Database

--- server.js
+++ server.js
@@ -88,8 +88,11 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     let found = false;
-    db.select('*').from('users').then(user => {
-        console.log(user);
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user => {
+        console.log(user)
     })
     if (!found) {
         res.status(400).json('Not Found')
